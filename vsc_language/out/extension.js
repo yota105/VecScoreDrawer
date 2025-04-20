064b68a446f505689f377b992772ca59458a599b
yota105 <yotayota105@gmail.com>
2025-04-21 03:24:20 +0900

    vsc-languageを追加 (仮)

---
+++ vsc_language/out/extension.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+function activate(context) {
+    console.log('VSC言語サポート拡張機能が有効化されました');
+}
+exports.activate = activate;
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
---
+++ vsc_language/src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('VSC言語サポート拡張機能が有効化されました');
+}
+
+export function deactivate() {}
